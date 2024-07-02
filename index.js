a9a74afd30ced97818804cd47810cf949c7edaf8
Avinash <avinashkmg1310@gmail.com>
2024-07-03 03:13:59 +0530

    minor styling correction

--- index.js
+++ index.js
@@ -7,112 +7,116 @@ const totalveggies = document.getElementById("total");
 
 //Handle Add to Vegetable Shop Submit
 function handleAddVegtoShop(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  let veggiesData = {
-    theVegName: vegName.value,
-    theVegPrice: vegPrice.value,
-    theVegQty: vegQty.value,
-  };
+	let veggiesData = {
+		theVegName: vegName.value,
+		theVegPrice: vegPrice.value,
+		theVegQty: vegQty.value,
+	};
 
-  axios
-    .post(
-      "https://crudcrud.com/api/dd627d9e4da944889e0b79a56b4519d7/vegShopData",
-      veggiesData
-    )
-    .then((response) => {
-      displayVeggiesList(response.data);
-      console.log(response);
-    })
-    .catch((error) => console.log(error));
+	axios
+		.post(
+			"https://crudcrud.com/api/03f96ed343be47b785947b9ebaef2a50/vegShopData",
+			veggiesData
+		)
+		.then((response) => {
+			displayVeggiesList(response.data);
+			console.log(response);
+		})
+		.catch((error) => console.log(error));
 
-  vegName.value = "";
-  vegPrice.value = "";
-  vegQty.value = "";
+	vegName.value = "";
+	vegPrice.value = "";
+	vegQty.value = "";
 }
 
 //Display Veg Lists on Page Refresh
 window.addEventListener("DOMContentLoaded", () => {
-  axios
-    .get(
-      "https://crudcrud.com/api/dd627d9e4da944889e0b79a56b4519d7/vegShopData"
-    )
-    .then((response) => {
-      for (let i = 0; i < response.data.length; i++) {
-        displayVeggiesList(response.data[i]);
-      }
-    })
-    .catch((error) => console.log(error));
+	axios
+		.get(
+			"https://crudcrud.com/api/03f96ed343be47b785947b9ebaef2a50/vegShopData"
+		)
+		.then((response) => {
+			for (let i = 0; i < response.data.length; i++) {
+				displayVeggiesList(response.data[i]);
+			}
+		})
+		.catch((error) => console.log(error));
 });
 
 //Create & Display Vegetable Lists on Screen
 function displayVeggiesList(vegObj) {
-  const vegList = document.createElement("li");
-  vegList.className = "vlist";
-  veggiesLists.appendChild(vegList);
+	const vegList = document.createElement("li");
+	vegList.className = "vlist";
+	veggiesLists.appendChild(vegList);
 
-  const vegDataDiv = document.createElement("div");
-  vegDataDiv.innerHTML = `<span class="me-3">${vegObj.theVegName}</span> <span  class="me-3"><strong>₹</strong>${vegObj.theVegPrice}</span>    <span>${vegObj.theVegQty}<strong>KG</strong></span>`;
-  vegList.appendChild(vegDataDiv);
+	const vegDataDiv = document.createElement("div");
+	vegDataDiv.innerHTML = `<span class="me-3">${vegObj.theVegName}</span> <span  class="me-3"><strong>₹</strong>${vegObj.theVegPrice}</span>   <span>${vegObj.theVegQty}<strong>KG</strong></span>`;
+	vegList.appendChild(vegDataDiv);
 
-  const btnDiv = document.createElement("div");
-  btnDiv.classList.add("btn-group", "gap-1", "ms-2");
-  vegList.appendChild(btnDiv);
+	const btnDiv = document.createElement("div");
+	btnDiv.classList.add("btn-group", "gap-1", "ms-2");
+	vegList.appendChild(btnDiv);
 
-  const buyVal = document.createElement("input");
-  buyVal.type = "number";
-  buyVal.classList.add("form-control");
-  btnDiv.appendChild(buyVal);
+	const buyVal = document.createElement("input");
+	buyVal.type = "number";
+	buyVal.classList.add("form-control");
+	btnDiv.appendChild(buyVal);
 
-  const buybtn = document.createElement("button");
-  buybtn.type = "button";
-  buybtn.classList.add("btn", "btn-outline-success");
-  buybtn.innerText = "Buy";
-  btnDiv.appendChild(buybtn);
+	const buybtn = document.createElement("button");
+	buybtn.type = "button";
+	buybtn.classList.add("btn", "btn-outline-success");
+	buybtn.innerText = "Buy";
+	btnDiv.appendChild(buybtn);
 
-  buybtn.addEventListener("click", () => {
-    // console.log(buyVal.value);
-    vegDataDiv.innerHTML = `${vegObj.theVegName}    <strong>₹</strong>${
-      vegObj.theVegPrice
-    }  ${vegObj.theVegQty - buyVal.value}  <strong>KG</strong>`;
+	buybtn.addEventListener("click", () => {
+		// console.log(buyVal.value);
+		vegDataDiv.innerHTML = `<span class="me-3">${
+			vegObj.theVegName
+		}</span> <span  class="me-3"><strong>₹</strong>${
+			vegObj.theVegPrice
+		}</span>   <span>${
+			vegObj.theVegQty - buyVal.value
+		}<strong>KG</strong></span>`;
 
-    let buyqty = vegObj.theVegQty - buyVal.value;
-    vegObj.theVegQty = buyqty;
+		let buyqty = vegObj.theVegQty - buyVal.value;
+		vegObj.theVegQty = buyqty;
 
-    axios
-      .put(
-        "https://crudcrud.com/api/dd627d9e4da944889e0b79a56b4519d7/vegShopData/" +
-          vegObj._id,
-        {
-          theVegName: vegObj.theVegName,
-          theVegPrice: vegObj.theVegPrice,
-          theVegQty: buyqty.toString(),
-        }
-      )
-      .then((response) => console.log(response.status))
-      .catch((error) => console.log(error));
-  });
+		axios
+			.put(
+				"https://crudcrud.com/api/03f96ed343be47b785947b9ebaef2a50/vegShopData/" +
+					vegObj._id,
+				{
+					theVegName: vegObj.theVegName,
+					theVegPrice: vegObj.theVegPrice,
+					theVegQty: buyqty.toString(),
+				}
+			)
+			.then((response) => console.log(response.status))
+			.catch((error) => console.log(error));
+	});
 
-  const deletebtn = document.createElement("button");
-  deletebtn.type = "button";
-  deletebtn.classList.add("btn", "btn-outline-danger", "btn-sm");
-  deletebtn.innerText = "Delete";
-  btnDiv.appendChild(deletebtn);
+	const deletebtn = document.createElement("button");
+	deletebtn.type = "button";
+	deletebtn.classList.add("btn", "btn-outline-danger", "btn-sm");
+	deletebtn.innerText = "Delete";
+	btnDiv.appendChild(deletebtn);
 
-  deletebtn.addEventListener("click", () => {
-    veggiesLists.removeChild(vegList);
-    totalveggies.innerHTML = `${
-      document.getElementsByClassName("vlist").length
-    }`;
+	deletebtn.addEventListener("click", () => {
+		veggiesLists.removeChild(vegList);
+		totalveggies.innerHTML = `${
+			document.getElementsByClassName("vlist").length
+		}`;
 
-    axios
-      .delete(
-        "https://crudcrud.com/api/dd627d9e4da944889e0b79a56b4519d7/vegShopData/" +
-          vegObj._id
-      )
-      .then((response) => console.log(response.status))
-      .catch((error) => console.log(error));
-  });
+		axios
+			.delete(
+				"https://crudcrud.com/api/03f96ed343be47b785947b9ebaef2a50/vegShopData/" +
+					vegObj._id
+			)
+			.then((response) => console.log(response.status))
+			.catch((error) => console.log(error));
+	});
 
-  totalveggies.innerHTML = `${document.getElementsByClassName("vlist").length}`;
+	totalveggies.innerHTML = `${document.getElementsByClassName("vlist").length}`;
 }
